682a76ecbba1d0fa39ca20b0baf993b35738080f
Althaaff <muhammadalthaf228@gmail.com>
2025-05-18 22:14:21 +0530

    feat: add protected routes for creating, updating, and deleting bags .

--- routes/admin.routes.js
+++ routes/admin.routes.js
@@ -1,9 +1,29 @@
 import express from "express";
 import { authenticate, authorizeAdmin } from "../middlewares/authMiddleWare.js";
-import { getAllUsers } from "../controllers/user.controller.js";
+import {
+  deleteUserById,
+  getAllUsers,
+  getCurrentUserById,
+  updateCurrentUserById,
+} from "../controllers/user.controller.js";
+import {
+  createBag,
+  deleteBag,
+  updateBag,
+} from "../controllers/bag.controller.js";
 
 const router = express.Router();
 
 router.get("/users", authenticate, authorizeAdmin, getAllUsers);
+router
+  .route("/:id")
+  .delete(authenticate, authorizeAdmin, deleteUserById)
+  .get(authenticate, authorizeAdmin, getCurrentUserById)
+  .put(authenticate, authorizeAdmin, updateCurrentUserById);
+
+// protected bags route (admin) :
+router.route("/createBag").post(authenticate, authorizeAdmin, createBag);
+router.route("/update/:id").put(authenticate, authorizeAdmin, updateBag);
+router.route("/delete/:id").delete(authenticate, authorizeAdmin, deleteBag);
 
 export default router;
